06889398008ca6ccb65df0f901945c62a724da8f
Prashantg30 <prashant.gupta@cloudstok.com>
2025-08-01 07:16:52 +0000

    names updated

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from 'express';
 const routes = Router();
 
 routes.get('/', async (req: Request, res: Response) => {
-  res.send({ status: true, msg: 'AirJet Backend Testing is up and running!! 👍' });
+  res.send({ status: true, msg: 'Aviator_2.0 Backend Testing is up and running!! 👍' });
 });
 
 export { routes };
