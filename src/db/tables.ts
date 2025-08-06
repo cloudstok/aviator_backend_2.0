6287dcdcacbe7b69b8ac29ec6b27f8ceb371b463
sushant81074 <sushant94601@gmail.com>
2025-08-06 13:37:01 +0000

    primary keys corrected

--- src/db/tables.ts
+++ src/db/tables.ts
@@ -8,7 +8,7 @@ export const lobbies = `CREATE TABLE IF NOT EXISTS lobbies (
    server_seed varchar(32) not null,
    hash varchar(128) not null,
    created_at datetime DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY USING BTREE (id),
+   PRIMARY KEY USING BTREE (id),
    index idx_lobby (lobby_id) USING BTREE
  );`
 
@@ -23,13 +23,13 @@ export const bets = `CREATE TABLE IF NOT EXISTS bets (
    auto_cashout  DECIMAL(10, 2) DEFAULT NULL,
    avatar INT NOT NULL,
    hash varchar(255) not null,
-    PRIMARY KEY USING BTREE (id),
+   PRIMARY KEY USING BTREE (id),
    created_at datetime DEFAULT CURRENT_TIMESTAMP
  );`
 
 
 export const settlement = `CREATE TABLE IF NOT EXISTS settlement (
-   id int primary key AUTO_INCREMENT,
+   id int AUTO_INCREMENT,
    bet_id varchar(255) DEFAULT NULL,
    lobby_id varchar(255) DEFAULT NULL,
    name varchar(255) NOT NULL,
@@ -43,7 +43,7 @@ export const settlement = `CREATE TABLE IF NOT EXISTS settlement (
    win_amount decimal(10, 2) NOT NULL default 0.00,
    status enum('cashout', 'crashed') DEFAULT 'crashed',
    created_at datetime DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY USING BTREE (id),
+   PRIMARY KEY USING BTREE (id),
    index idx_user_operator (user_id, operator_id) USING BTREE,
    index idx_user_operator_lobby (user_id, operator_id, lobby_id) USING BTREE
 );`
@@ -62,12 +62,12 @@ export const roundStats = ` CREATE TABLE IF NOT EXISTS round_stats (
    biggest_looser decimal(10, 2) DEFAULT 0.00,
    total_round_settled decimal(10, 2) DEFAULT 0.00,
    created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY USING BTREE (id),
+   PRIMARY KEY USING BTREE (id),
    index idx_lobby (lobby_id) USING BTREE
  );`
 
 export const user_messages = `CREATE TABLE user_messages (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     user_id VARCHAR(255) NOT NULL,
     operator_id varchar(255) DEFAULT NULL,
     avatar INT not null,
@@ -75,6 +75,5 @@ export const user_messages = `CREATE TABLE user_messages (
     msg TEXT,
     gif varchar(255) DEFAULT null,
     user_likes TEXT DEFAULT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-     PRIMARY KEY USING BTREE (id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );`
